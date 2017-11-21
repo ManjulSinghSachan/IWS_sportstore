21dca823daaecd8a7e5ce73883764c506289ac73
Manjul Singh Sachan <sachanmanjul@gmail.com>
2017-11-21 17:46:59 +0530

    Changed database details

--- connect.php
+++ connect.php
@@ -1,7 +1,7 @@
 <?php 
-	$username="root";
-	$password="";
-	$servername="localhost";
+	$username="sql12206252";
+	$password="qDhsLVHUV4";
+	$servername="sql12.freemysqlhosting.net:3306";
 	$connection= mysql_connect($servername,$username,$password);
 	$select_db=mysql_select_db('sportskart');
 	$db_error="";

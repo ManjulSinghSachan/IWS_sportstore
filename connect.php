9435d0cfd3e12a3243d57023bbe64668c90870a0
Manjul Singh Sachan <sachanmanjul@gmail.com>
2017-11-22 23:11:43 +0530

    updated procfile

--- connect.php
+++ connect.php
@@ -4,7 +4,7 @@
 	$password="qDhsLVHUV4";
 	$servername="sql12.freemysqlhosting.net:3306";
 	$connection= mysqli_connect($servername,$username,$password);
-	$select_db=mysqli_select_db('sql12206252');
+	$select_db=mysqli_select_db($connection, 'sql12206252');
 	$db_error="";
 	if(!$connection||!$select_db){
 		$db_error="Cannot connect to DB";

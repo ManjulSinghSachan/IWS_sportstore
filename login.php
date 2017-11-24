bbf6efede2a4e52d629ced9910dc2a7c94478230
Manjul Singh Sachan <sachanmanjul@gmail.com>
2017-11-24 23:46:55 +0530

    Fix in login

--- login.php
+++ login.php
@@ -6,27 +6,27 @@ $error="";
 if(isset($_POST['email'])&&isset($_POST['pwd'])){
 		$log_in_email=$_POST['email'];
 		$log_in_pwd=$_POST['pwd'];
-		//echo $log_in_email;
-		//echo $log_in_pwd;
-		$query_new="SELECT user_id FROM login WHERE name='".mysql_real_escape_string($log_in_email)."' AND pwd='".mysql_real_escape_string($log_in_pwd)."'";
-		if($ret_new=mysql_query($query_new)){
+		echo $log_in_email;
+		echo $log_in_pwd;
+		$query_new="SELECT user_id FROM login WHERE name='".mysqli_real_escape_string($connect, $log_in_email)."' AND pwd='".mysqli_real_escape_string($connect, $log_in_pwd)."'";
+		if($ret_new=mysqli_query($connect, $query_new)){
 			$debug= "Query Executed";
-			//echo $ret_new;
-			$num_rows=mysql_num_rows($ret_new);
+			echo $ret_new;
+			$num_rows=mysqli_num_rows($ret_new);
 			if($num_rows==0)
 				$error= "Invalid UserName/Password combination";
 			else{
 				//echo "ID grabbed";
-				$user_id=mysql_result($ret_new,0,'user_id');
+				$user_id=mysqli_result($ret_new,0,'user_id');
 				//echo $user_id;
 				$_SESSION["user_id"]=$user_id;
 			//	echo "SESSION ID".$_SESSION['user_id'];
 				header("Location:home.php");
 			}
 		}
-		
-		
-	
+
+
+
 	}
 
 

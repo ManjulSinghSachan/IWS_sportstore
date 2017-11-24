5d5e93667e23c3829f5cfba790c2a446df81b598
Manjul Singh Sachan <sachanmanjul@gmail.com>
2017-11-25 00:30:25 +0530

    Fix in login

--- login.php
+++ login.php
@@ -8,9 +8,9 @@ if(isset($_POST['email'])&&isset($_POST['pwd'])){
 		$log_in_pwd=$_POST['pwd'];
 		//echo $log_in_email;
 		//echo $log_in_pwd;
-		$query_new="SELECT user_id FROM login WHERE email='".mysqli_real_escape_string($connect,$log_in_email)."' AND pwd='".mysqli_real_escape_string($connect,$log_in_pwd)."';";
+		$query_new="SELECT user_id FROM login WHERE email='".mysqli_real_escape_string($connection,$log_in_email)."' AND pwd='".mysqli_real_escape_string($connection,$log_in_pwd)."';";
 		//echo $query_new;
-		if($ret_new=mysqli_query($connect, $query_new)){
+		if($ret_new=mysqli_query($connection, $query_new)){
 			$debug= "Query Executed";
 			echo $debug;
 			$num_rows=mysqli_num_rows($ret_new);

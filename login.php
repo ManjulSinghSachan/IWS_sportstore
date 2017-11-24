4fd8f2a3ac1990911f02d5c408a3d9dc349f8ec3
Manjul Singh Sachan <sachanmanjul@gmail.com>
2017-11-25 00:27:02 +0530

    Fix in login

--- login.php
+++ login.php
@@ -8,17 +8,18 @@ if(isset($_POST['email'])&&isset($_POST['pwd'])){
 		$log_in_pwd=$_POST['pwd'];
 		//echo $log_in_email;
 		//echo $log_in_pwd;
-		$query_new="SELECT user_id FROM login WHERE email='".$log_in_email."' AND pwd='".$log_in_pwd."';";
+		$query_new="SELECT user_id FROM login WHERE email='".mysqli_real_escape_string($connect,$log_in_email)."' AND pwd='".mysqli_real_escape_string($connect,$log_in_pwd)."';";
 		//echo $query_new;
 		if($ret_new=mysqli_query($connect, $query_new)){
 			$debug= "Query Executed";
-			//echo $ret_new;
+			echo $debug;
 			$num_rows=mysqli_num_rows($ret_new);
 			if($num_rows==0)
 				$error= "Invalid UserName/Password combination";
 			else{
 				//echo "ID grabbed";
-				$user_id=mysqli_result($ret_new,0,'user_id');
+				$row=mysqli_fetch_assoc($ret_new);
+				$user_id=$row['user_id'];
 				//echo $user_id;
 				$_SESSION["user_id"]=$user_id;
 			//	echo "SESSION ID".$_SESSION['user_id'];

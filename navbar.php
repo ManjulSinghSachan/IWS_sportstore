5599b34e1b8e54c24d3bab57dd50d459b79b94b8
Manjul Singh Sachan <sachanmanjul@gmail.com>
2017-11-29 17:31:14 +0530

    navbar css changes

--- navbar.php
+++ navbar.php
@@ -137,7 +137,7 @@ if(!logged_in()){
             <div class="navbar-collapse collapse" id="navigation">
 
                 <ul class="nav navbar-nav navbar-left">
-                    <li class="active"><a href="home.php" style="color:white">Home</a>
+                    <li class="active"><a href="home.php">Home</a>
                     </li>
                     <li class="dropdown yamm-fw">
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown" data-hover="dropdown" data-delay="200">Men <b class="caret"></b></a>
